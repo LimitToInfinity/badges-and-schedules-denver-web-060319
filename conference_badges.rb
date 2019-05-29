85f84e11d283491c0a885d295d36bf48347091ae
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-29 23:23:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  return "Hello, my name is #{name}." 
+end
+
+def batch_badge_creator(i)
+  for i in ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"] do
+    puts badge_maker(i)
+  end
+end
