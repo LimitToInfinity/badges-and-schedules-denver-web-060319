e2abf75dc4dc288fe2929bd28b74241cc5929979
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-29 23:43:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,5 +9,5 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
-  array.collect { |n| "Hello, #{n}! You'll be assigned to room #{array[n]}!" }
+  array.collect { |n| "Hello, #{n}! You'll be assigned to room #{array.index(n)}!" }
 end
