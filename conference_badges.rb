a23207f082c6ae8c260c0c53d3762fad7c33cc6c
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-29 23:47:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,13 +1,15 @@
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
 def badge_maker(name)
   return "Hello, my name is #{name}." 
 end
 
-def batch_badge_creator(array)
-  array.collect { |n| badge_maker(n)}
+def batch_badge_creator(attendees)
+  attendees.collect { |n| badge_maker(n)}
 end
 
 def assign_rooms(array)
-  array.collect { |n| "Hello, #{n}! You'll be assigned to room #{array.index(n).to_i + 1}!" }
+  attendees.collect { |n| "Hello, #{n}! You'll be assigned to room #{array.index(n).to_i + 1}!" }
+end
+
+def printer(attendees)
+  puts batch_badge_creator + assign_rooms
 end
