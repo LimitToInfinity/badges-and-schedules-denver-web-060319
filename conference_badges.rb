4d9db51fb1c954dcc1ae0eb1547bf7460a37d0c5
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-30 00:07:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,6 +11,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees.fetch(1..n))
-  puts assign_rooms(attendees.fetch(1..n))
+  puts batch_badge_creator(attendees.fetch(0..attendees.length))
+  puts assign_rooms(attendees.fetch(0..attendees.length))
 end
